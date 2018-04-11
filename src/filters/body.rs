7efcdccbb3e78eb6b718ff543031cc1ec4432b08
Sean McArthur <sean@seanmonstar.com>
2018-04-11 13:35:42 -0700

    start of warp::body::json

--- src/filters/body.rs
+++ src/filters/body.rs
@@ -8,15 +8,31 @@
 //! let a = warp::body::concat();
 //! let b = warp::body::concat();
 //!
-//! // Cannot chain something after 'a'
+//! // Cannot 'and' chain something after 'a'
 //! a.and(b)
 //! ```
+
+use std::marker::PhantomData;
+
+use futures::{Async, Future, Poll, Stream};
+use futures::stream::Concat2;
+use hyper::{Body, Chunk};
+use serde::de::DeserializeOwned;
+use serde_json;
+
 use ::filter::Filter;
 use ::route::Route;
+use ::Error;
 
 pub fn concat() -> Concat {
     Concat {
         _i: (),
+    }
+}
+
+pub fn json<T: DeserializeOwned>() -> Json<T> {
+    Json {
+        _marker: PhantomData,
     }
 }
 
@@ -25,13 +41,63 @@ pub struct Concat {
     _i: (),
 }
 
-pub struct ConcatFut;
+pub struct ConcatFut {
+    fut: Concat2<Body>,
+}
 
 impl Filter for Concat {
     type Extract = ConcatFut;
 
     fn filter<'a>(&self, route: Route<'a>) -> Option<(Route<'a>, Self::Extract)> {
         route.take_body()
-            .map(|(route, _body)| (route, ConcatFut))
+            .map(|(route, body)| (route, ConcatFut {
+                fut: body.unwrap().concat2(),
+            }))
+    }
+}
+
+impl Future for ConcatFut {
+    type Item = Chunk;
+    type Error = Error;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        self.fut.poll()
+            .map_err(|e| unimplemented!("concat error: {}", e))
+    }
+}
+
+pub struct Json<T> {
+    _marker: PhantomData<T>,
+}
+
+pub struct JsonFut<T> {
+    concat: ConcatFut,
+    _marker: PhantomData<T>,
+}
+
+impl<T> Filter for Json<T> {
+    type Extract = JsonFut<T>;
+
+    fn filter<'a>(&self, route: Route<'a>) -> Option<(Route<'a>, Self::Extract)> {
+        route.take_body()
+            .map(|(route, body)| (route, JsonFut {
+                concat: ConcatFut {
+                    fut: body.unwrap().concat2(),
+                },
+                _marker: PhantomData,
+            }))
+    }
+}
+
+impl<T> Future for JsonFut<T>
+where
+    T: DeserializeOwned,
+{
+    type Item = T;
+    type Error = Error;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        let buf = try_ready!(self.concat.poll());
+        Ok(Async::Ready(serde_json::from_slice(&buf).unwrap()))
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 extern crate crossbeam_channel;
-extern crate frunk_core;
-extern crate futures;
+#[macro_use] extern crate futures;
 extern crate http;
 extern crate hyper;
 #[macro_use] extern crate log;
+extern crate serde;
+extern crate serde_json;
 
 mod blocking;
+mod error;
 mod filter;
 mod filters;
 mod reply;
@@ -14,6 +16,7 @@ mod server;
 pub mod test;
 
 pub use self::blocking::{blocking, blocking_new};
+pub use self::error::Error;
 pub use self::filter::Filter;
 pub use self::filters::{
     body,
