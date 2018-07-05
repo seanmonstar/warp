82a8636f0e56594a404c7e2fa04067746b084e8d
Sean McArthur <sean@seanmonstar.com>
2018-07-05 10:48:34 -0700

    use published hyper again

--- src/server.rs
+++ src/server.rs
@@ -42,7 +42,6 @@ where
             })
         };
         let srv = HyperServer::bind(&addr.into())
-            .pipeline()
             .serve(service);
         info!("warp drive engaged: listening on {}", srv.local_addr());
 
