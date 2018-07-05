a15b05dd4a0a9d734077ebbcba6e0656715196a5
Sean McArthur <sean@seanmonstar.com>
2018-07-05 10:48:01 -0700

    seal the Reply trait

--- src/reply.rs
+++ src/reply.rs
@@ -1,14 +1,12 @@
-//! dox?
+//! Create responses to reply to requests.
 
-use futures::{future, Future};
 use http;
 use http::header::{CONTENT_TYPE, HeaderValue};
 use hyper::Body;
 use serde::Serialize;
 use serde_json;
 
-use ::filter::{Cons, Either};
-use ::never::Never;
+pub(crate) use self::sealed::{Reply, Reply_, Response};
 
 /// Easily convert a type into a `Response`.
 #[inline]
@@ -19,24 +17,6 @@ where
     Reply_(Response::new(Body::from(val)))
 }
 
-/// dox?
-pub struct Reply_(Response);
-
-impl Reply for Reply_ {
-    type Future = future::FutureResult<Response, Never>;
-
-    fn into_response(self) -> Self::Future {
-        future::ok(self.0)
-    }
-}
-
-/// Easily constructor a 400 Bad Request response.
-pub fn client_error() -> Response {
-    http::Response::builder()
-        .status(400)
-        .body(Body::empty())
-        .unwrap()
-}
 
 /// Convert the value into a `Response` with the value encoded as JSON.
 pub fn json<T>(val: T) -> Reply_
@@ -63,117 +43,119 @@ where
     })
 }
 
-/// dox?
-pub type Response = http::Response<Body>;
-/*
-/// An HTTP response used by Warp servers.
-#[derive(Debug)]
-pub struct Response(pub(crate) http::Response<Body>);
+// Seal the `Reply` trait and the `Reply_` wrapper type for now.
+mod sealed {
+    use futures::{future, Future};
+    use hyper::Body;
 
-impl From<&'static str> for Response {
-    #[inline]
-    fn from(s: &'static str) -> Response {
-        Response(http::Response::new(Body::from(s)))
+    use ::filter::{Cons, Either};
+    use ::never::Never;
+
+    // A trait describing the various things that a Warp server can turn into a `Response`.
+    pub trait Reply {
+        /// The future of the Response.
+        type Future: Future<Item=Response, Error=Never> + Send + 'static;
+        /// Convert self into `Self::Future`.
+        fn into_response(self) -> Self::Future;
     }
-}
 
-impl From<String> for Response {
-    fn from(s: String) -> Response {
-        Response(http::Response::new(Body::from(s)))
-    }
-}
-*/
+    /// dox?
+    pub struct Reply_(pub(super) Response);
 
-impl<T, U> From<Either<T, U>> for Response
-where
-    Response: From<T> + From<U>,
-{
-    fn from(either: Either<T, U>) -> Response {
-        match either {
-            Either::A(a) => Response::from(a),
-            Either::B(b) => Response::from(b),
+    impl Reply for Reply_ {
+        type Future = future::FutureResult<Response, Never>;
+
+        fn into_response(self) -> Self::Future {
+            future::ok(self.0)
         }
     }
-}
 
-impl<T> From<Cons<T>> for Response
-where
-    Response: From<T>,
-{
-    fn from(cons: Cons<T>) -> Response {
-        Response::from(cons.0)
-    }
-}
+    pub type Response = ::http::Response<Body>;
 
-/// A trait describing the various things that a Warp server can turn into a `Response`.
-pub trait Reply {
-    /// The future of the Response.
-    type Future: Future<Item=Response, Error=Never> + Send + 'static;
-    /// Convert self into `Self::Future`.
-    fn into_response(self) -> Self::Future;
-}
-
-/*
-impl Reply for Response {
-    type Future = future::FutureResult<Response, Never>;
-    #[inline]
-    fn into_response(self) -> Self::Future {
-        future::ok(self)
-    }
-}
-
-impl IntoFuture for Response {
-    type Item = Response;
-    type Error = Never;
-    type Future = future::FutureResult<Response, Never>;
-    #[inline]
-    fn into_future(self) -> Self::Future {
-        future::ok(self)
-    }
-}
-*/
-
-impl<T: Reply, U: Reply> Reply for Either<T, U> {
-    type Future = future::Either<T::Future, U::Future>;
-    #[inline]
-    fn into_response(self) -> Self::Future {
-        match self {
-            Either::A(a) => future::Either::A(a.into_response()),
-            Either::B(b) => future::Either::B(b.into_response()),
+    impl<T, U> From<Either<T, U>> for Response
+    where
+        Response: From<T> + From<U>,
+    {
+        fn from(either: Either<T, U>) -> Response {
+            match either {
+                Either::A(a) => Response::from(a),
+                Either::B(b) => Response::from(b),
+            }
         }
     }
-}
 
-impl<T> Reply for Cons<T>
-where
-    T: Reply
-{
-    type Future = T::Future;
-    #[inline]
-    fn into_response(self) -> Self::Future {
-        self.0.into_response()
+    impl<T> From<Cons<T>> for Response
+    where
+        Response: From<T>,
+    {
+        fn from(cons: Cons<T>) -> Response {
+            Response::from(cons.0)
+        }
     }
-}
 
-impl<T, R, E> Reply for T
-where
-    T: Future<Item=R, Error=E> + Send + 'static,
-    R: Reply + 'static,
-    E: Reply + 'static,
-{
-    type Future = future::Then<T, future::Either<R::Future, E::Future>, fn(Result<R, E>) -> future::Either<R::Future, E::Future>>;
-    fn into_response(self) -> Self::Future {
-        self.then(|result| match result {
-            Ok(reply) => future::Either::A(reply.into_response()),
-            Err(err) => future::Either::B(err.into_response()),
-        })
+
+    /*
+    impl Reply for Response {
+        type Future = future::FutureResult<Response, Never>;
+        #[inline]
+        fn into_response(self) -> Self::Future {
+            future::ok(self)
+        }
     }
-}
 
-impl Reply for ::never::Never {
-    type Future = future::FutureResult<Response, Never>;
-    fn into_response(self) -> Self::Future {
-        match self {}
+    impl IntoFuture for Response {
+        type Item = Response;
+        type Error = Never;
+        type Future = future::FutureResult<Response, Never>;
+        #[inline]
+        fn into_future(self) -> Self::Future {
+            future::ok(self)
+        }
+    }
+    */
+
+    impl<T: Reply, U: Reply> Reply for Either<T, U> {
+        type Future = future::Either<T::Future, U::Future>;
+        #[inline]
+        fn into_response(self) -> Self::Future {
+            match self {
+                Either::A(a) => future::Either::A(a.into_response()),
+                Either::B(b) => future::Either::B(b.into_response()),
+            }
+        }
+    }
+
+    impl<T> Reply for Cons<T>
+    where
+        T: Reply
+    {
+        type Future = T::Future;
+        #[inline]
+        fn into_response(self) -> Self::Future {
+            self.0.into_response()
+        }
+    }
+
+    impl<T, R, E> Reply for T
+    where
+        T: Future<Item=R, Error=E> + Send + 'static,
+        R: Reply + 'static,
+        E: Reply + 'static,
+    {
+        type Future = future::Then<T, future::Either<R::Future, E::Future>, fn(Result<R, E>) -> future::Either<R::Future, E::Future>>;
+        fn into_response(self) -> Self::Future {
+            self.then(|result| match result {
+                Ok(reply) => future::Either::A(reply.into_response()),
+                Err(err) => future::Either::B(err.into_response()),
+            })
+        }
+    }
+
+    impl Reply for ::never::Never {
+        type Future = future::FutureResult<Response, Never>;
+        fn into_response(self) -> Self::Future {
+            match self {}
+        }
     }
 }
 
