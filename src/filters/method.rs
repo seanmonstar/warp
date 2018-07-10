c032e10ffca52a7460c59d5a7b1e0453219bb329
Sean McArthur <sean@seanmonstar.com>
2018-07-10 12:41:50 -0700

    make method filters implement Copy

--- src/filters/method.rs
+++ src/filters/method.rs
@@ -1,47 +1,47 @@
 use http::Method;
 
-use ::filter::{And, Cons, Filter, FilterClone, filter_fn, filter_fn_cons, HList};
+use ::filter::{And, Cons, Filter, filter_fn, filter_fn_cons, HList};
 use ::never::Never;
 use ::reject::{CombineRejection, Rejection};
 
 /// Wrap a `Filter` in a new one that requires the request method to be `GET`.
-pub fn get<F>(filter: F) -> And<impl FilterClone<Extract=(), Error=Rejection>, F>
+pub fn get<F>(filter: F) -> And<impl Filter<Extract=(), Error=Rejection> + Copy, F>
 where
     F: Filter + Clone,
     F::Extract: HList,
     F::Error: CombineRejection<Rejection>,
 {
-    method_is(Method::GET).and(filter)
+    method_is(|| &Method::GET).and(filter)
 }
 
 /// Wrap a `Filter` in a new one that requires the request method to be `POST`.
-pub fn post<F>(filter: F) -> And<impl FilterClone<Extract=(), Error=Rejection>, F>
+pub fn post<F>(filter: F) -> And<impl Filter<Extract=(), Error=Rejection> + Copy, F>
 where
     F: Filter + Clone,
     F::Extract: HList,
     F::Error: CombineRejection<Rejection>,
 {
-    method_is(Method::POST).and(filter)
+    method_is(|| &Method::POST).and(filter)
 }
 
 /// Wrap a `Filter` in a new one that requires the request method to be `PUT`.
-pub fn put<F>(filter: F) -> And<impl FilterClone<Extract=(), Error=Rejection>, F>
+pub fn put<F>(filter: F) -> And<impl Filter<Extract=(), Error=Rejection> + Copy, F>
 where
     F: Filter + Clone,
     F::Extract: HList,
     F::Error: CombineRejection<Rejection>,
 {
-    method_is(Method::PUT).and(filter)
+    method_is(|| &Method::PUT).and(filter)
 }
 
 /// Wrap a `Filter` in a new one that requires the request method to be `DELETE`.
-pub fn delete<F>(filter: F) -> And<impl FilterClone<Extract=(), Error=Rejection>, F>
+pub fn delete<F>(filter: F) -> And<impl Filter<Extract=(), Error=Rejection> + Copy, F>
 where
     F: Filter + Clone,
     F::Extract: HList,
     F::Error: CombineRejection<Rejection>,
 {
-    method_is(Method::DELETE).and(filter)
+    method_is(|| &Method::DELETE).and(filter)
 }
 
 /// Extract the `Method` from the request.
@@ -51,10 +51,14 @@ pub fn method() -> impl Filter<Extract=Cons<Method>, Error=Never> + Copy {
     })
 }
 
-fn method_is(method: Method) -> impl FilterClone<Extract=(), Error=Rejection> {
+fn method_is<F>(func: F) -> impl Filter<Extract=(), Error=Rejection> + Copy
+where
+    F: Fn() -> &'static Method + Copy,
+{
     filter_fn(move |route| {
+        let method = func();
         trace!("method::{:?}?: {:?}", method, route.method());
-        if route.method() == &method {
+        if route.method() == method {
             Ok(())
         } else {
             //TODO: return method-specific error
