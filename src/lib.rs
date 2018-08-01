f7f215e2a3a7fad32d40229fc8ba9768ccfc9df3
Sean McArthur <sean@seanmonstar.com>
2018-08-01 14:38:00 -0700

    v0.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/warp/0.0.0")]
+#![doc(html_root_url = "https://docs.rs/warp/0.1.0")]
 #![deny(missing_docs)]
 #![deny(missing_debug_implementations)]
 #![cfg_attr(test, deny(warnings))]
