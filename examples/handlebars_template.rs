1c2f16c9468477a98f6e851fc9d24bceae10eab5
João Oliveira <hello@jxs.pt>
2018-08-03 11:29:30 +0100

    add Handlebars templating engine example usage, closes #1

---
+++ examples/handlebars_template.rs
@@ -0,0 +1,43 @@
+#![deny(warnings)]
+extern crate warp;
+extern crate hyper;
+extern crate handlebars;
+#[macro_use]
+extern crate serde_json;
+
+use warp::Filter;
+use handlebars::Handlebars;
+use std::sync::Arc;
+
+fn main() {
+    let template = "<!DOCTYPE html>
+                    <html>
+                      <head>
+                        <title>Warp Handlebars template example</title>
+                      </head>
+                      <body>
+                        <h1>Hello {{user}}!</h1>
+                      </body>
+                    </html>";
+    
+    let mut hb = Handlebars::new();
+    // register the template
+    hb.register_template_string("template.html", template).unwrap();
+
+    // Turn Handlebars instance into a Filter so we can combine it
+    // easily with others...
+    let hb = Arc::new(hb);
+    let hb = warp::any().map(move || hb.clone());
+
+    //GET /
+    let route = warp::get(
+        warp::path::index().and(hb).map(index)
+    );
+
+    warp::serve(route).run(([127, 0, 0, 1], 3030));
+}
+
+//GET / handler
+fn index(hb: Arc<Handlebars>) -> impl warp::Reply {
+    hb.render("template.html", &json!({"user": "Warp"})).unwrap()
+}
