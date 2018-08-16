b6a927d3850494e451cfe5a9c0674332d7d07526
Sean McArthur <sean@seanmonstar.com>
2018-08-16 10:28:27 -0700

    revert hello example to basic hello world

--- examples/hello.rs
+++ examples/hello.rs
@@ -5,19 +5,10 @@ use warp::Filter;
 
 fn main() {
     // Match any request and return hello world!
-    /*
     let routes = warp::any()
         .map(|| "Hello, World!");
-        */
-    let text = warp::path("plaintext")
-        .map(|| "Hello, World!");
-    let json = warp::path("json")
-        .map(|| warp::reply::json(&["Hello, World"]));
-    let routes = text.or(json);
-
 
     warp::serve(routes)
-        .unstable_pipeline()
         .run(([127, 0, 0, 1], 3030));
 }
 
