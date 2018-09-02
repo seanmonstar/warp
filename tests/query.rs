cf8621aad75240d7f81fb055528ae457bd468e95
Rasmus Kaj <kaj@kth.se>
2018-09-02 17:29:52 +0200

    Treat no query string as empty query string
    Fix #86 in the place where it is easy to fix.  Also added a bunch of
    tests for missing query strings (and query string missing required
    pieces).

--- tests/query.rs
+++ tests/query.rs
@@ -1,5 +1,7 @@
 #![deny(warnings)]
 extern crate warp;
+#[macro_use]
+extern crate serde_derive;
 
 use std::collections::HashMap;
 
@@ -13,6 +15,84 @@ fn query() {
     let extracted = req.filter(&as_map).unwrap();
     assert_eq!(extracted["foo"], "bar");
     assert_eq!(extracted["baz"], "quux");
+}
+
+#[test]
+fn query_struct() {
+    let as_struct = warp::query::<MyArgs>();
+
+    let req = warp::test::request()
+        .path("/?foo=bar&baz=quux");
+
+    let extracted = req.filter(&as_struct).unwrap();
+    assert_eq!(extracted, MyArgs { foo: Some("bar".into()), baz: Some("quux".into()) });
+}
+
+#[test]
+fn empty_query_struct() {
+    let as_struct = warp::query::<MyArgs>();
+
+    let req = warp::test::request()
+        .path("/?");
+
+    let extracted = req.filter(&as_struct).unwrap();
+    assert_eq!(extracted, MyArgs { foo: None, baz: None });
+}
+
+#[test]
+fn missing_query_struct() {
+    let as_struct = warp::query::<MyArgs>();
+
+    let req = warp::test::request()
+        .path("/");
+
+    let extracted = req.filter(&as_struct).unwrap();
+    assert_eq!(extracted, MyArgs { foo: None, baz: None });
+}
+
+#[derive(Deserialize, Debug, Eq, PartialEq)]
+struct MyArgs {
+    foo: Option<String>,
+    baz: Option<String>,
+}
+
+#[test]
+fn required_query_struct() {
+    let as_struct = warp::query::<MyRequiredArgs>();
+
+    let req = warp::test::request()
+        .path("/?foo=bar&baz=quux");
+
+    let extracted = req.filter(&as_struct).unwrap();
+    assert_eq!(extracted, MyRequiredArgs { foo: "bar".into(), baz: "quux".into() });
+}
+
+#[test]
+fn missing_required_query_struct_partial() {
+    let as_struct = warp::query::<MyRequiredArgs>();
+
+    let req = warp::test::request()
+        .path("/?foo=something");
+
+    let extracted = req.filter(&as_struct);
+    assert!(extracted.is_err())
+}
+
+#[test]
+fn missing_required_query_struct_no_query() {
+    let as_struct = warp::query::<MyRequiredArgs>();
+
+    let req = warp::test::request()
+        .path("/");
+
+    let extracted = req.filter(&as_struct);
+    assert!(extracted.is_err())
+}
+
+#[derive(Deserialize, Debug, Eq, PartialEq)]
+struct MyRequiredArgs {
+    foo: String,
+    baz: String,
 }
 
 #[test]
