11f95e622d54bc51d3e6b4235d25eddc391a8f87
Sean McArthur <sean@seanmonstar.com>
2018-09-18 13:23:09 -0700

    add warp::reply::with::headers(HeaderMap) filter
    Closes #88

--- tests/reply_with.rs
+++ tests/reply_with.rs
@@ -2,6 +2,7 @@
 extern crate pretty_env_logger;
 extern crate warp;
 
+use warp::http::header::{HeaderMap, HeaderValue};
 use warp::Filter;
 
 #[test]
@@ -21,6 +22,34 @@ fn header() {
         .map(warp::reply)
         .with(prev_header)
         .with(header);
+
+    let req = warp::test::request();
+    let resp = req.reply(&yes_header);
+    assert_eq!(resp.headers()["foo"], "bar", "replaces header");
+}
+
+#[test]
+fn headers() {
+    let mut headers = HeaderMap::new();
+    headers.insert("server", HeaderValue::from_static("warp"));
+    headers.insert("foo", HeaderValue::from_static( "bar"));
+
+    let headers = warp::reply::with::headers(headers);
+
+    let no_header = warp::any()
+        .map(warp::reply)
+        .with(&headers);
+
+    let req = warp::test::request();
+    let resp = req.reply(&no_header);
+    assert_eq!(resp.headers()["foo"], "bar");
+    assert_eq!(resp.headers()["server"], "warp");
+
+    let prev_header = warp::reply::with::header("foo", "sean");
+    let yes_header = warp::any()
+        .map(warp::reply)
+        .with(prev_header)
+        .with(headers);
 
     let req = warp::test::request();
     let resp = req.reply(&yes_header);
