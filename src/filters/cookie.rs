5c6e0a7f27d2a88392a2086b6587f8faa9ea8e32
Sean McArthur <sean@seanmonstar.com>
2018-09-28 13:58:42 -0700

    make use of typed headers internally

--- src/filters/cookie.rs
+++ src/filters/cookie.rs
@@ -1,9 +1,9 @@
 //! Cookie Filters
 
-use http::header::HeaderValue;
+use headers::Cookie;
 
 use ::never::Never;
-use ::filter::{Filter, One};
+use ::filter::{Filter, One, filter_fn_one};
 use ::reject::Rejection;
 use super::header;
 
@@ -11,9 +11,13 @@ use super::header;
 ///
 /// If found, extracts the value of the cookie, otherwise rejects.
 pub fn cookie(name: &'static str) -> impl Filter<Extract=One<String>, Error=Rejection> + Copy {
-    header::value(&::http::header::COOKIE, move |val| {
-        find_cookie(name, val)
-    })
+    header::header2()
+        .and_then(move |cookie: Cookie| {
+            cookie
+                .get(name)
+                .map(String::from)
+                .ok_or_else(|| ::reject::bad_request())
+        })
 }
 
 /// Creates a `Filter` that looks for an optional cookie by name.
@@ -21,47 +25,33 @@ pub fn cookie(name: &'static str) -> impl Filter<Extract=One<String>, Error=Reje
 /// If found, extracts the value of the cookie, otherwise continues
 /// the request, extracting `None`.
 pub fn optional(name: &'static str) -> impl Filter<Extract=One<Option<String>>, Error=Never> + Copy {
-    header::optional_value(&::http::header::COOKIE, move |val| {
-        find_cookie(name, val)
-    })
+    header::optional()
+        .map(move |opt: Option<Cookie>| {
+            opt.and_then(|cookie| {
+                cookie
+                    .get(name)
+                    .map(String::from)
+            })
+        })
 }
 
-/// Creates a `Filter` that looks for an optional cookie by name and maps the value.
-///
-/// If found, extracts the value of the cookie and applies the provided function to it,
-/// otherwise continues the request, extracting `None`.
+#[doc(hidden)]
+#[deprecated(note = "optional filters will be generalized")]
 pub fn optional_value<U, F>(name: &'static str, func: F)
     -> impl Filter<Extract=One<Option<U>>, Error=Never> + Copy
 where
     F: Fn(&str) -> U + Copy,
     U: Send,
 {
-    header::optional_value(&::http::header::COOKIE, move |val| {
-        find_cookie(name, val).map(|v| func(v.as_ref()))
+    use headers::HeaderMapExt;
+    filter_fn_one(move |route| {
+        Ok(route.headers()
+            .typed_get()
+            .and_then(|cookie: Cookie| {
+                cookie
+                    .get(name)
+                    .map(func)
+            }))
     })
 }
 
-//TODO: probably shouldn't extract a `String`, but rather a `Cookie`.
-//That would allow use to change from cloning a `String` to just shallow cloning
-//the `Bytes` of the header value...
-fn find_cookie(name: &str, value: &HeaderValue) -> Option<String> {
-    value
-        .to_str()
-        .ok()
-        .and_then(|value| {
-            //TODO: could be optimized, and there's edge cases not handled...
-            for pair in value.split(';') {
-                let pair = pair.trim();
-                // name.len() + `=`
-                if pair.len() > name.len() + 1 {
-                    if pair.starts_with(name) {
-                        if pair.as_bytes()[name.len()] == b'=' {
-                            return Some(pair[name.len() + 1..].to_string())
-                        }
-                    }
-                }
-            }
-
-            None
-        })
-}
