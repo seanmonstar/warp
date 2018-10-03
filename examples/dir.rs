a5e9166921b9178270b434695264a6b29144ba7f
Robert Masen <r.f.masen@gmail.com>
2018-10-03 12:56:43 -0500

    Add automatic index.html insert for directory routes  (#110)
    Closes #108

---
+++ examples/dir.rs
@@ -0,0 +1,10 @@
+#![deny(warnings)]
+extern crate pretty_env_logger;
+extern crate warp;
+
+fn main() {
+    pretty_env_logger::init();
+
+    warp::serve(warp::fs::dir("examples/dir"))
+        .run(([127, 0, 0, 1], 3030));
+}
