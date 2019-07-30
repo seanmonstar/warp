36e1d99fe91103cb7fc7ebbd85f7d3a5426b668e
Simon Sapin <simon.sapin@exyr.org>
2019-07-30 21:27:17 +0200

    Update tungstenite and serde_urlencoded

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/warp/0.1.18")]
+#![doc(html_root_url = "https://docs.rs/warp/0.1.19")]
 #![deny(missing_docs)]
 #![deny(missing_debug_implementations)]
 #![cfg_attr(test, deny(warnings))]
