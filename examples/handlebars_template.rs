dca4d8432d1ff74cac980d4421f3a4703939bffb
John <john.douglas@mycostech.com>
2019-12-26 11:36:43 -0700

    Fixed formatting, really

--- examples/handlebars_template.rs
+++ examples/handlebars_template.rs
@@ -15,7 +15,8 @@ fn render<T>(template: WithTemplate<T>, hbs: Arc<Handlebars>) -> impl warp::Repl
 where
     T: Serialize,
 {
-    let render = hbs.render(template.name, &template.value)
+    let render = hbs
+        .render(template.name, &template.value)
         .unwrap_or_else(|err| err.to_string());
     warp::reply::html(render)
 }
