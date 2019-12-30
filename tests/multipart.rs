da74792e1805b93410bdf4fb84893b57f63a7987
Sean McArthur <sean@seanmonstar.com>
2019-12-30 11:47:59 -0800

    Change multipart stream API backwards compatibility hazards (#358)

--- tests/multipart.rs
+++ tests/multipart.rs
@@ -1,18 +1,23 @@
 #![deny(warnings)]
-use futures::{FutureExt, StreamExt, TryStreamExt};
-use warp::Filter;
+use bytes::BufMut;
+use futures::{TryFutureExt, TryStreamExt};
+use warp::{multipart, Filter};
 
 #[tokio::test]
 async fn form_fields() {
     let _ = pretty_env_logger::try_init();
 
-    let route = warp::multipart::form().and_then(|form: warp::multipart::FormData| {
+    let route = multipart::form().and_then(|form: multipart::FormData| {
         async {
             // Collect the fields into (name, value): (String, Vec<u8>)
             let part: Result<Vec<(String, Vec<u8>)>, warp::Rejection> = form
                 .and_then(|part| {
                     let name = part.name().to_string();
-                    part.concat().map(move |value| Ok((name, value)))
+                    let value = part.stream().try_fold(Vec::new(), |mut vec, data| {
+                        vec.put(data);
+                        async move { Ok(vec) }
+                    });
+                    value.map_ok(move |vec| (name, vec))
                 })
                 .try_collect()
                 .await
