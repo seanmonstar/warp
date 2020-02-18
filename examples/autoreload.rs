3bb923a2eb0e0801fa79ec5e867b0f5d4922b54c
Adrian Kappel <atkappel@gmail.com>
2020-02-18 13:46:12 -0500

    Fix fmt check errors

--- examples/autoreload.rs
+++ examples/autoreload.rs
@@ -1,8 +1,8 @@
 #![deny(warnings)]
-use std::convert::Infallible;
-use warp::Filter;
 use hyper::server::Server;
 use listenfd::ListenFd;
+use std::convert::Infallible;
+use warp::Filter;
 
 extern crate listenfd;
 /// You'll need to install `systemfd` and `cargo-watch`:
@@ -22,9 +22,7 @@ async fn main() {
     // useful shortly). Thus, we'll need to convert our `warp::Filter` into
     // a `hyper::service::MakeService` for use with a `hyper::server::Server`.
     let svc = warp::service(routes);
-    let make_svc = hyper::service::make_service_fn(|_: _| async move {
-        Ok::<_, Infallible>(svc)
-    });
+    let make_svc = hyper::service::make_service_fn(|_: _| async move { Ok::<_, Infallible>(svc) });
 
     let mut listenfd = ListenFd::from_env();
     // if listenfd doesn't take a TcpListener (i.e. we're not running via
@@ -33,7 +31,7 @@ async fn main() {
     let server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         Server::from_tcp(l).unwrap()
     } else {
-        Server::bind(&([127,0,0,1], 3030).into())
+        Server::bind(&([127, 0, 0, 1], 3030).into())
     };
 
     server.serve(make_svc).await.unwrap();
