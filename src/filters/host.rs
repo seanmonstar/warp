9a248d6cdaeda54fa262ae92fe74e0ff01cfe84b
Dillen Meijboom <info@dmeijboom.nl>
2020-03-20 22:39:43 +0100

    Implemented host type

--- src/filters/host.rs
+++ src/filters/host.rs
@@ -1,28 +1,33 @@
 //! Host filter
 //!
-use crate::filter::{Filter, filter_fn, one};
+use std::str::FromStr;
+use crate::filter::{Filter, filter_fn_one, One};
 use crate::reject::{self, Rejection};
 use futures::future;
+use http::uri::Authority;
 
-/// Get the host of the route
-pub fn host() -> impl Filter<Extract = (Option<String>,), Error = Rejection> + Copy {
+/// Represents the hostname, returned by the `host()` filter.
+#[derive(Debug)]
+pub struct Host(Authority);
+
+/// Try to get the host of the route
+pub fn host() -> impl Filter<Extract = One<Option<Host>>, Error = Rejection> + Copy {
     let name = "Host";
 
-    filter_fn(move |route| {
-        future::ok(one(
+    filter_fn_one(move |route| {
+        future::ok(
             route.uri()
                 .authority()
-                .and_then(|authority| Some(authority.host()))
-                .and_then(|host| Some(host.to_string()))
-                .or_else(|| {
+                .and_then(|authority| Some(Host(authority.clone())))
+                .ok_or_else(|| {
                     route.headers()
                         .get(name)
                         .ok_or_else(|| reject::missing_header(name))
                         .and_then(|value| value.to_str().map_err(|_| reject::invalid_header(name)))
-                        .and_then(|s| Ok(String::from(s)))
-                        .ok()
+                        .and_then(|value| Authority::from_str(value).map_err(|_| reject::invalid_header(name)))
+                        .and_then(|authority| Ok(Host(authority)))
                 })
-            )
+                .ok()
         )
     })
 }
