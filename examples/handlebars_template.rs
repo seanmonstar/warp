09ce0a86049b4b896149170b829e44044e7f78e5
Draphar <draphar-code@protonmail.com>
2020-03-23 18:18:19 +0100

    Improve the Handlebars example
    The boilerplate is unnecessary.

--- examples/handlebars_template.rs
+++ examples/handlebars_template.rs
@@ -1,25 +1,8 @@
 #![deny(warnings)]
-use std::sync::Arc;
-
 use handlebars::Handlebars;
-use serde::Serialize;
 use serde_json::json;
+use std::sync::Arc;
 use warp::Filter;
-
-struct WithTemplate<T: Serialize> {
-    name: &'static str,
-    value: T,
-}
-
-fn render<T>(template: WithTemplate<T>, hbs: Arc<Handlebars>) -> impl warp::Reply
-where
-    T: Serialize,
-{
-    let render = hbs
-        .render(template.name, &template.value)
-        .unwrap_or_else(|err| err.to_string());
-    warp::reply::html(render)
-}
 
 #[tokio::main]
 async fn main() {
@@ -34,25 +17,28 @@ async fn main() {
                     </html>";
 
     let mut hb = Handlebars::new();
-    // register the template
+    // Register the template
     hb.register_template_string("template.html", template)
         .unwrap();
 
-    // Turn Handlebars instance into a Filter so we can combine it
-    // easily with others...
+    // Turn the Handlebars instance into a Filter
+    // so we can combine it easily with others...
     let hb = Arc::new(hb);
 
     // Create a reusable closure to render template
-    let handlebars = move |with_template| render(with_template, hb.clone());
+    let render = move |name| {
+        // A closure that takes a `Serialize`able value,
+        // and passes it into the template engine
+        move |value| hb.render(name, &value).unwrap_or_else(|e| e.to_string())
+    };
 
     //GET /
     let route = warp::get()
         .and(warp::path::end())
-        .map(|| WithTemplate {
-            name: "template.html",
-            value: json!({"user" : "Warp"}),
-        })
-        .map(handlebars);
+        .map(|| json!({
+                "user" : "Warp"
+        }))
+        .map(render("template.html"));
 
     warp::serve(route).run(([127, 0, 0, 1], 3030)).await;
 }
