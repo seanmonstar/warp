4cf69fa18e79e0da2842e7c096a9532f6e37927d
Taiki Endo <te316e89@gmail.com>
2020-06-06 06:36:28 +0900

    Remove uses of pin_project::project attribute
    pin-project will deprecate the project attribute due to some unfixable
    limitations.
    
    Refs: https://github.com/taiki-e/pin-project/issues/225

--- src/filter/and.rs
+++ src/filter/and.rs
@@ -3,7 +3,7 @@ use std::pin::Pin;
 use std::task::{Context, Poll};
 
 use futures::ready;
-use pin_project::{pin_project, project};
+use pin_project::pin_project;
 
 use super::{Combine, Filter, FilterBase, HList, Internal, Tuple};
 use crate::reject::CombineRejection;
@@ -41,7 +41,7 @@ pub struct AndFuture<T: Filter, U: Filter> {
     state: State<T, U>,
 }
 
-#[pin_project]
+#[pin_project(project = StateProj)]
 enum State<T: Filter, U: Filter> {
     First(#[pin] T::Future, U),
     Second(Option<T::Extract>, #[pin] U::Future),
@@ -59,17 +59,15 @@ where
             <<<T::Extract as Tuple>::HList as Combine<<U::Extract as Tuple>::HList>>::Output as HList>::Tuple,
         <U::Error as CombineRejection<T::Error>>::One>;
 
-    #[project]
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
         loop {
             let pin = self.as_mut().project();
-            #[project]
             let (ex1, fut2) = match pin.state.project() {
-                State::First(first, second) => match ready!(first.poll(cx)) {
+                StateProj::First(first, second) => match ready!(first.poll(cx)) {
                     Ok(first) => (first, second.filter(Internal)),
                     Err(err) => return Poll::Ready(Err(From::from(err))),
                 },
-                State::Second(ex1, second) => {
+                StateProj::Second(ex1, second) => {
                     let ex2 = match ready!(second.poll(cx)) {
                         Ok(second) => second,
                         Err(err) => return Poll::Ready(Err(From::from(err))),
@@ -78,7 +76,7 @@ where
                     self.set(AndFuture { state: State::Done });
                     return Poll::Ready(Ok(ex3));
                 }
-                State::Done => panic!("polled after complete"),
+                StateProj::Done => panic!("polled after complete"),
             };
 
             self.set(AndFuture {
