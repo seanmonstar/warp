25e837e651a6ff50ff81014cea7131b6d0a1f105
Henry Zimmerman <hzimmerm@akamai.com>
2020-07-08 15:46:13 -0400

    improve doc comments

--- src/filter/wrap.rs
+++ src/filter/wrap.rs
@@ -1,11 +1,32 @@
 use super::Filter;
 
-/// Wraps filters.
+/// Types that allow customizing Filters by wrapping them.
+///
+/// Types that implement `Wrap` can be used with the `.with()` filter combinator.
+///
+/// Notable uses of this trait include:
+///
+/// - `warp::filters::log::Log`
+/// - `warp::filters::compression::Compression`
+/// - `warp::filters::cors::Cors`
+/// - `warp::filters::cors::Builder`
+/// - `warp::filters::reply::WithHeader`
+/// - `warp::filters::reply::WithHeaderS`
+/// - `warp::filters::reply::WithDefaultHeader`
+///
+/// # Example
+/// ```
+/// // use warp::{Filter, Wrap};
+///
+/// // let route = warp::any()
+/// //    .map(warp::reply)
+/// //    .with(unimplemented!());
+/// ```
 pub trait Wrap<F: Filter> {
     /// The type of the Filter produced by wrapping another Filter.
     type Wrapped: Filter;
 
-    /// Wraps the filter.
+    /// Wraps the Filter with the associated type that implements Filter.
     fn wrap(&self, filter: F) -> Self::Wrapped;
 }
 
