95ad70e3bae4c0cee67fe8a176f833ffaa2e5fdb
Sean McArthur <sean@seanmonstar.com>
2020-08-31 12:57:17 -0700

    v0.2.5

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/warp/0.2.4")]
+#![doc(html_root_url = "https://docs.rs/warp/0.2.5")]
 #![deny(missing_docs)]
 #![deny(missing_debug_implementations)]
 #![cfg_attr(test, deny(warnings))]
