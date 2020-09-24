4d6147389ae97b37ab2d166933954bbed33545b1
valbendan <valbendan@hotmail.com>
2020-09-24 17:50:35 +0800

    fix unix_socket example compile error due to not impl Transport

--- examples/unix_socket.rs
+++ examples/unix_socket.rs
@@ -1,13 +1,15 @@
 #![deny(warnings)]
 
+use futures::TryStreamExt;
 use tokio::net::UnixListener;
+use warp::LiftIo;
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
 
     let mut listener = UnixListener::bind("/tmp/warp.sock").unwrap();
-    let incoming = listener.incoming();
+    let incoming = listener.incoming().map_ok(LiftIo).into_stream();
     warp::serve(warp::fs::dir("examples/dir"))
         .run_incoming(incoming)
         .await;
