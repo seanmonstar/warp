586053352b64c00c2cbe02a4082facee6628e47f
Fuyang Liu <fuyangl@spotify.com>
2020-10-24 12:58:12 +0200

    Refactory

--- examples/dyn_reply.rs
+++ examples/dyn_reply.rs
@@ -1,11 +1,20 @@
 #![deny(warnings)]
+
+use serde_derive::{Deserialize, Serialize};
 use warp::{http::StatusCode, Filter};
 
+#[derive(Deserialize, Serialize, Clone)]
+struct Value {
+    value: String,
+}
+
 async fn dyn_reply(word: String) -> Result<Box<dyn warp::Reply>, warp::Rejection> {
-    if &word == "hello" {
-        Ok(Box::new("world"))
-    } else {
-        Ok(Box::new(StatusCode::BAD_REQUEST))
+    match word.as_str() {
+        "hello" => Ok(Box::new("world")), // how to reply "world" with different status code
+        "world" => Ok(Box::new(warp::reply::json(&Value {
+            value: "Good".to_string(),
+        }))), // how to reply json with different status code
+        _ => Ok(Box::new(StatusCode::BAD_REQUEST)),
     }
 }
 
--- examples/http_kv.rs
+++ examples/http_kv.rs
@@ -4,8 +4,10 @@ use std::env;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
-use warp::reply::Json;
-use warp::{reject, Filter, Rejection};
+use warp::{http::StatusCode, Filter, Rejection, Reply};
+
+// Use simple map as an in memory KV store
+type Map = Arc<Mutex<HashMap<String, Value>>>;
 
 #[derive(Deserialize, Serialize, Clone)]
 struct Value {
@@ -32,9 +34,6 @@ async fn main() {
         env::set_var("RUST_LOG", "http_kv=info");
     }
     pretty_env_logger::init();
-
-    // Use simple map as an in memory KV store
-    type Map = Arc<Mutex<HashMap<String, Value>>>;
     let map: Map = Arc::new(Mutex::new(HashMap::new()));
 
     let map_clone = map.clone();
@@ -42,38 +41,43 @@ async fn main() {
         .and(warp::path::param::<String>())
         .and(warp::body::content_length_limit(1024 * 16).and(warp::body::json()))
         .and(warp::any().map(move || map_clone.clone()))
-        .and_then(|key, value: Value, map: Map| async move {
-            // use `move` keyword to let the closure owns key and value
-            match map.lock().await.insert(key, value.clone()) {
-                Some(_) => Ok(warp::reply::json(&value)) as Result<Json, Rejection>, // Otherwise error: cannot infer type for type parameter `E` declared on the enum `Result`
-                None => Ok(warp::reply::json(&value)), // TODO: return 201 here, and 200 above
-            }
-        });
+        .and_then(|key: String, value: Value, map: Map| post_handler(key, value, map));
 
     let map_clone = map.clone();
     let get = warp::get()
         .and(warp::path::param::<String>())
         .and(warp::any().map(move || map_clone.clone()))
-        .and_then(|key: String, map: Map| async move {
-            match map.lock().await.get(key.as_str()) {
-                Some(value) => Ok(warp::reply::json(value)),
-                None => Err(reject::not_found()), // TODO: why not getting not found but a `HTTP method not allowed`?
-            }
-        });
+        .and_then(|key: String, map: Map| get_handler(key, map));
 
     let map_clone = map.clone();
     let delete = warp::delete()
         .and(warp::path::param::<String>())
         .and(warp::any().map(move || map_clone.clone()))
-        .and_then(|key: String, map: Map| async move {
-            match map.lock().await.remove(key.as_str()) {
-                Some(value) => Ok(warp::reply::json(&value)),
-                None => Err(reject::not_found()), // TODO: why not getting not found but a `HTTP method not allowed`?
-            }
-        });
+        .and_then(|key: String, map: Map| delete_handler(key, map));
 
     // View access logs by setting `RUST_LOG=todos`.
     let routes = get.or(post).or(delete).with(warp::log("http_kv"));
     // Start up the server...
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+}
+
+async fn post_handler(key: String, value: Value, map: Map) -> Result<Box<dyn Reply>, Rejection> {
+    match map.lock().await.insert(key, value.clone()) {
+        Some(_) => Ok(Box::new(warp::reply::json(&value))),
+        None => Ok(Box::new(warp::reply::json(&value))), // TODO: return 201 here, and 200 above
+    }
+}
+
+async fn get_handler(key: String, map: Map) -> Result<Box<dyn Reply>, Rejection> {
+    match map.lock().await.get(key.as_str()) {
+        Some(value) => Ok(Box::new(warp::reply::json(value).into_response())),
+        None => Ok(Box::new(StatusCode::NOT_FOUND)),
+    }
+}
+
+async fn delete_handler(key: String, map: Map) -> Result<Box<dyn Reply>, Rejection> {
+    match map.lock().await.remove(key.as_str()) {
+        Some(value) => Ok(Box::new(warp::reply::json(&value).into_response())),
+        None => Ok(Box::new(StatusCode::NOT_FOUND)),
+    }
 }
