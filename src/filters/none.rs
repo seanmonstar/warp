6b76c75d8ddc4bc83af207cd8e96d35a98e02b8e
aaron-hardin <aaronrhardin@gmail.com>
2020-11-01 19:06:45 -0600

    Fix formatting

--- src/filters/none.rs
+++ src/filters/none.rs
@@ -16,7 +16,7 @@ use crate::reject::{self, Rejection};
 /// let route_1 = warp::path!("something").map(|| "Hello, world!".to_string());
 /// let route_2 = warp::path!("something2").map(|| "Hello, world again!".to_string());
 /// let route_3 = warp::path!("something3").map(|| "Hello, world again again!".to_string());
-/// 
+///
 /// let routes = warp::none()
 ///     .or(route_1)
 ///     .or(route_2)
