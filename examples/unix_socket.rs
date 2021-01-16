54c66925c2cd1f4bbe67d6e3afe8b5e146f7647c
valbendan <valbendan@hotmail.com>
2021-01-16 09:45:33 +0800

    fix cargo fmt

--- examples/unix_socket.rs
+++ examples/unix_socket.rs
@@ -2,15 +2,17 @@
 
 use futures::TryStreamExt;
 use tokio::net::UnixListener;
-use warp::LiftIo;
 use tokio_stream::wrappers::UnixListenerStream;
+use warp::LiftIo;
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
 
     let listener = UnixListener::bind("/tmp/warp.sock").unwrap();
-    let incoming = UnixListenerStream::new(listener).map_ok(LiftIo).into_stream();
+    let incoming = UnixListenerStream::new(listener)
+        .map_ok(LiftIo)
+        .into_stream();
     warp::serve(warp::fs::dir("examples/dir"))
         .run_incoming(incoming)
         .await;
