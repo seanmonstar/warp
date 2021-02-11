1f4cf1abe44ce853caf264101b4cfc393ba14614
comath <sven.cattell@elastic.co>
2021-02-10 18:39:55 -0800

    I did not know that about

--- src/reply.rs
+++ src/reply.rs
@@ -435,11 +435,7 @@ where
     fn into_response(self) -> Response {
         match self {
             Ok(t) => t.into_response(),
-            Err(e) => {
-                // Maybe panic?
-                tracing::error!("Infallible Error: {:?}", e);
-                StatusCode::INTERNAL_SERVER_ERROR.into_response()
-            }
+            Err(e) => match e {}
         }
     }
 }
