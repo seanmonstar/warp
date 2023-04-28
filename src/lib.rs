220be141b218ffd9a4e4959429ca6fdafaad6c7a
Sean McArthur <sean@seanmonstar.com>
2023-04-28 11:21:59 -0400

    v0.3.5

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/warp/0.3.4")]
+#![doc(html_root_url = "https://docs.rs/warp/0.3.5")]
 #![deny(missing_docs)]
 #![deny(missing_debug_implementations)]
 #![deny(rust_2018_idioms)]
